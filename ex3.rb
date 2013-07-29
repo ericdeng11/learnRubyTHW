f2ad8ddeea0f6adb8804b7ae33f9cf2ba9f986b8
QiangDeng <ericdeng11@gmail.com>
2013-07-28 22:13:18 -0400

    add some files

---
+++ ex3.rb
@@ -0,0 +1,15 @@
+puts "I will now count my chickens:"
+puts 25 * 3 % 4
+puts "Hens", 25 + 30 / 6
+puts "Roosters ", 100 - 25 * 3 % 4
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7 ?"
+puts 3 + 2 < 5 - 7
+
+puts "Is it greater? " , 5 > -2
+puts 5 >= -2
+puts 5 <= -2
+
+puts "try" , 5 + 2,"best",  3 + 7
