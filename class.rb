964a594444922cc8007c48e2a8fc54a4e565787f
QiangDeng <ericdeng11@gmail.com>
2013-08-02 11:17:33 -0400

    another test

---
+++ class.rb
@@ -0,0 +1,23 @@
+class MyFirstClass < Object
+end
+
+class Try
+  def initialize(name, loc)
+    @name = name
+    @loc = loc
+    @where
+    puts "Yes?"
+  end
+  def fun_in_obj()
+      @where = 10
+      z = 10;
+      puts z / @where
+      puts "#{z/@where}"
+      puts @name
+      puts "what"
+  end
+end
+
+monster = Try.new("Eric", "NY")
+monster.fun_in_obj
+
