77dc5b63eae2a680b344efaa093ca211f97debaa
QiangDeng <ericdeng11@gmail.com>
2013-08-16 11:41:29 -0400

    b

--- code/binarySearch.rb
+++ code/binarySearch.rb
@@ -1,5 +1,6 @@
 my_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-
+arr = Array.new
+arr << -11 << -7 << 0 << 10 << 100 << 1000 << 10000
 
 def binary_search(array, left, right, key)
     if( left > right )
@@ -19,3 +20,5 @@ x = my_array.length - 1
 puts binary_search(my_array, 0, x, 5)
 puts binary_search(my_array, 0 , x, 0)
 puts binary_search(my_array, 0, my_array.length - 1, 3)
+puts binary_search(arr, 0, arr.length - 1 , -7)
+puts binary_search(arr, 0, arr.length - 1, 100)
